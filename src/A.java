f707028097b76167365cb5fc29857eadfe9b2905
M8IN <M8IN@LAPTOP-820ERDM2>
2023-08-24 22:03:41 +0530

    second coomment

--- src/A.java
+++ src/A.java
@@ -3,7 +3,8 @@ public class A {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Atul");
+System.out.println("Atul first comment");
+System.out.println(" second comment");
 	}
 
 }
